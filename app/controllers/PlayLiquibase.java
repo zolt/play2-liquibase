6c2e4122bffbee48ea857fbdd2a22922e9ff0c2e
zolt <timochkinea@gmail.com>
2014-03-16 15:45:12 +0700

    initial commit

---
+++ app/controllers/PlayLiquibase.java
@@ -0,0 +1,11 @@
+package controllers;
+
+/**
+ * Created by zolt on 15.03.14.
+ */
+public class PlayLiquibase {
+
+    public static void log(String message) {
+        System.out.println(message);
+    }
+}
